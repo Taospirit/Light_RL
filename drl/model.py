70caa22f6553fb7248a70b036b203cbffe4fb35e
joelrliu <joelrliu@tencent.com>
2024-05-10 16:49:15 +0800

    feat: add MLP implement(refer)

--- drl/model.py
+++ drl/model.py
@@ -235,3 +235,21 @@ class HardSwish(nn.Module): # 一种激活函数
     def forward(self, x):
         return self.relu6(x + 3.) / 6. * x
 
+class MLP(nn.Module):
+    """ Very simple multi-layer perceptron (also called FFN)
+        refer: https://github.com/facebookresearch/detr/blob/29901c51d7fe8712168b8d0d64351170bc0f83e0/models/detr.py#L289
+        default: input_dim -> output_dim
+        param: input_dim -> [ReLu(hidden_dim)] * (num_layers - 1) -> output_dim
+    """
+
+    def __init__(self, input_dim, hidden_dim, output_dim, num_layers=1):
+        super().__init__()
+        self.num_layers = num_layers
+        h = [hidden_dim] * (num_layers - 1)
+        self.layers = nn.ModuleList(nn.Linear(n, k) for n, k in zip([input_dim] + h, h + [output_dim]))
+
+    def forward(self, x):
+        for i, layer in enumerate(self.layers):
+            x = F.relu(layer(x)) if i < self.num_layers - 1 else layer(x)
+        return x
+    
